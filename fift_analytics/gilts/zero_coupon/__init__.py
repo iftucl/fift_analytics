23e2161852923721719b6b115a3bde4975ef6e68
uceslc0 <uceslc0@ucl.ac.uk>
2025-02-18 20:59:54 +0000

    adding first commit before release

--- fift_analytics/gilts/zero_coupon/__init__.py
+++ fift_analytics/gilts/zero_coupon/__init__.py
@@ -7,10 +7,11 @@ GILTS Zero Coupon Bonds
 from fift_analytics.gilts.zero_coupon.zc_pricers import get_zero_coupon_gilt_price
 from fift_analytics.gilts.zero_coupon.zc_duration import calculate_zero_coupon_bond_duration
 from fift_analytics.gilts.zero_coupon.zc_convexity import calculate_zero_coupon_bond_convexity
-
+from fift_analytics.gilts.zero_coupon.zc_duration import calculate_zero_coupon_bond_duration
 
 __all__ = [
     "get_zero_coupon_gilt_price",
     "calculate_zero_coupon_bond_duration",
     "calculate_zero_coupon_bond_convexity",
+    "calculate_zero_coupon_bond_duration",
 ]
